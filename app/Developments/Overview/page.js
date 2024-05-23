d82b0016d168385883caaa55f6f2c2c5d721c4a3
Naaadyy <nadiah.syafiqah@360geoinfo.com>
2024-05-23 15:36:17 +0800

    update

--- app/Developments/Overview/page.js
+++ app/Developments/Overview/page.js
@@ -1,22 +1,22 @@
-'use client';
-import React from 'react';
-import { useRouter } from 'next/router';
+// 'use client';
+// import React from 'react';
+// import { useRouter } from 'next/router';
 
-const OverviewPage = () => {
-  const router = useRouter();
+// const OverviewPage = () => {
+//   const router = useRouter();
 
-  const handleOverviewClick = () => {
-    router.push('/overview'); // Replace with the correct path to your Overview page
-  };
+//   const handleOverviewClick = () => {
+//     router.push('/overview'); // Replace with the correct path to your Overview page
+//   };
 
-  return (
-    <button
-      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2'
-      onClick={handleOverviewClick}
-    >
-      View Overview
-    </button>
-  );
-}
+//   return (
+//     <button
+//       className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2'
+//       onClick={handleOverviewClick}
+//     >
+//       View Overview
+//     </button>
+//   );
+// }
 
-export default OverviewPage;
+// export default OverviewPage;
