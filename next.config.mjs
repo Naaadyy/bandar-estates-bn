b654214db6d82afcbafde3207f5d127eebaa3a06
Zulfitri Othman <zulfitriothman31@gmail.com>
2024-06-06 15:55:15 +0800

    Video update testing

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,15 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/bandar-estates-bn', // Replace 'bandar-estates-bn' with your repository name
+  assetPrefix: '/bandar-estates-bn/', // Replace 'bandar-estates-bn' with your repository name
   webpack(config) {
     config.module.rules.push({
       test: /\.(mp4|webm|ogg|swf|ogv)$/,
       use: {
         loader: 'file-loader',
         options: {
-          publicPath: '/_next/static/videos/',
+          publicPath: 'https://github.com/Naaadyy/bandar-estates-bn/tree/main/public/videos', // Update the public path
           outputPath: 'static/videos/',
           name: '[name].[hash].[ext]',
           esModule: false,
